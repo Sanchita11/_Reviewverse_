964abf00fa35a60ac5b313b0da27fe7d7fcde126
MuditRst <52821308+MuditRst@users.noreply.github.com>
2021-05-13 01:26:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,27 +3,41 @@ const bodyParser = require("body-parser")
 const {spawn} = require('child_process')
 const { json } = require("body-parser")
 const { stringify } = require("qs")
-const path = require('path')
 
 login = false
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
+let datatosend = []
+let movie_name_send = ''
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine','ejs')
+
+app.use(express.static(__dirname + '/public'));
 
 app.get("/",function(req,res){
     if(login === true){
-        res.sendFile(path.join(__dirname, '/app.html'))
+        res.render("index")
     }else{
         res.redirect('/login')
     }
 })
 
 app.post("/",function(req,res){
-
     movie_name_send = req.body.movie_name
-    let datatosend = []
-    
+    res.redirect('/search')
+})
+
+
+app.get('/login',function(req,res){
+    res.render("login")
+})
+
+app.post('/login',function(req,res){
+    login = true;
+    res.redirect('/')
+})
+
+app.get('/search',function(req,res){
     const python = spawn('python',['app.py'])
 
     python.stdin.write(JSON.stringify(movie_name_send))
@@ -37,7 +51,7 @@ app.post("/",function(req,res){
         if(datatosend.length < 0){
             res.send("No result Found!")
         }
-        res.send(datatosend)
+        res.render("search_results",{movie_search : movie_name_send, movies:datatosend})
     })
 
     console.log(JSON.stringify(movie_name_send))
@@ -45,21 +59,8 @@ app.post("/",function(req,res){
     python.stdin.end()
 })
 
-app.get('/login',function(req,res){
-    res.sendFile(path.join(__dirname, '/login.html'))
-})
-
-app.post('/login',function(req,res){
-    login = true;
-    res.redirect('/')
-})
-
-app.get('/search',function(req,res){
-    res.sendFile(path.join(__dirname, '/search_results.html'))
-})
-
 app.get('/index',function(req,res){
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.render("index")
 })
 
 app.listen(3000,function(){
